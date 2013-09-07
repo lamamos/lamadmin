d06cba366a631d1b2776dca9bd82f7c82cb26d46
karlito <karlito@martobre.fr>
2013-09-07 13:10:30 +0200

    First commit, we have the two colones.

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+
+    </head>
+    <link rel="stylesheet" href="include/jquery-ui.css">
+    <link rel="stylesheet" href="include/style.css">
+    <script type="text/javascript" src="include/jquery-2.0.3.min.js"></script>
+    <script src="include/jquery-ui.js"></script>
+    <body>
+        <div id="sideBar">
+        
+            <? echo "la batte de menu"; ?>
+        </div>
+    
+        <div id="mainPannel">
+        
+            <? echo "le panneau d'amin en lui même <br> et ca roxxx"; ?> 
+        </div>    
+    </body>
+</html>
