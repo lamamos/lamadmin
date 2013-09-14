34e199c02c71557b32bc3cb6c3d7b8a26eeda0e8
karlito <karlito@martobre.fr>
2013-09-14 14:31:48 +0200

    Added some animation when changing from on pannel to an other. Close #14.

--- index.php
+++ index.php
@@ -80,77 +80,11 @@ $config = new Configuration();
         //this function is called at te opening of the page
         $(function() {  
             $("#mainPannel").tabs({
-                
                 beforeActivate: function(event, ui){
                     
-                    try{if(tabName){oldTabName = tabName;}}catch(e){oldTabName = "general";}
                     tabName = ui.newTab.attr('id').substr(1);
-                    activeSubModule = tabName;
-                    
-                    var data = {
-                        moduleName: activeModule,
-                        subModuleName: tabName,
-                    }
-                
-                    request = $.ajax({
-                        url: "/ajax/getFormSubModule.php",
-                        type: "POST",
-                        data: data
-                    });
-                    
-                    request.done(function(response, textStatus, jqXHR){
-                        
-
-                        //This line is useless as far as we commented the line 14380 in jquery-ui.js
-                        //Now jquery don't hide the old tab, we do it with this animation.
-                        //$("#forms #"+oldTabName+"_old").show();
-                        $("#forms div").addClass("pt-page-flipOutLeft");
-                        $("#forms div").one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-        
-                            //alert("delete");
-                            $("#forms div").removeClass("pt-page-flipOutLeft");
-                            $("#forms div").remove();
-                            $("#forms").append("<div id=\""+tabName+"\" class=\"subModuleForm\">"+response+"</div>");
-                            $("#forms #"+tabName).addClass("pt-page-flipInRight");
-                            $("#forms #"+tabName).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-            
-                                    $("#forms #"+tabName).removeClass("pt-page-flipInRight");
-                            });
-                        });
-                        
-                        
-                        
-                        
-                        $(".instanceForm").submit(function (){       
-
-                            var data = $(this).serialize();
-                            data += "&moduleName="+activeModule;
-
-                            $.ajax({
-                                type    : "POST",
-                                url     : "/ajax/setFormInstance.php",
-                                data    : data,
-                                success : function(data) {
-                                    //alert("done");
-                                    //opts.onSuccess.call(FORM[0], data);
-                                },
-                                error   : function() {
-                                    //opts.onError.call(FORM[0]);
-                                }
-                            });
-                        });
-                        
-                        
-                    });
-                    
-                    request.fail(function(jqXHR, textStatus, errorThrown){
-                    
-                        alert("error when getting the list of the submodule");
-                    });
-                    
-                    request.always(function(){});
+                    changeTab(tabName);
                 }
-            
             });
         });
         
@@ -172,21 +106,35 @@ $config = new Configuration();
             });
         
             request.done(function(response, textStatus, jqXHR){
-                $("#tabs").remove();
-                $("#forms").remove();
-                $("#mainPannel").append("<div id=\"forms\">"+response+"</div>");
-                $(".userForm").submit(function (){            
-                    $.ajax({
-                        type    : "POST",
-                        url     : "/ajax/setFormUser.php",
-                        data    : $(this).serialize(),
-                        success : function(data) {
-                            //alert("done");
-                            //opts.onSuccess.call(FORM[0], data);
-                        },
-                        error   : function() {
-                            //opts.onError.call(FORM[0]);
-                        }
+            
+                if($("#mainPannel #tabs").length > 0){   //if we have a tabs element
+                    
+                    $("#tabs").addClass("pt-page-moveToTop"); //we make the tab deseapear
+                    $("#tabs li").addClass("pt-page-moveToTop"); //we make the tab deseapear
+                    //when the tabs have desapeared
+                    $("#tabs li").one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+                        
+                        $("#tabs").remove(); //we delete them
+                        $("#tabs li").remove(); //we delete them                        
+                    });
+                }
+                
+                //we make the forms deseapear
+                $("#mainPannel #forms").addClass("pt-page-moveToBottom");
+                //when they are not anymore on the screen
+                $("#mainPannel #forms").one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+                
+                    $("#mainPannel #forms").remove();   //we delete them
+                    $("#mainPannel").append("<div id=\"forms\">"+response+"</div>");    //we add the user form
+                    $("#mainPannel #forms").addClass("pt-page-moveFromTop");    //we make it appeare
+                    $(".userForm").submit(function (){  //we make it a proper user form with an ajax handle
+                        $.ajax({
+                            type    : "POST",
+                            url     : "/ajax/setFormUser.php",
+                            data    : $(this).serialize(),
+                            success : function(data) {},
+                            error   : function() {}
+                        });
                     });
                 });
             });
@@ -218,19 +166,54 @@ $config = new Configuration();
         
             request.done(function(response, textStatus, jqXHR){
         
-                var tabs= response.split(","); 
-        
-                $("#tabs").remove();
-                $("#forms").remove();
-                $("#mainPannel").append("<ul id=\"tabs\"></ul>\n<div id=\"forms\"></div>");
-        
-                for(i=0; i<tabs.length; i++){
-        
-                    $("#tabs").append("<li id=\"#"+tabs[i]+"\"><a href=\"#"+tabs[i]+"\">"+tabs[i]+"</a></li>");
-                    $("#forms").append("<div id=\""+tabs[i]+"\"></div>");
+                var tabs= response.split(",");                 
+                
+                if($("#mainPannel #tabs").length > 0){   //if we have a tabs element
+                    
+                    $("#tabs li").addClass("pt-page-moveToBottom"); //we make the tab deseapear
+                    //when the tabs have desapeared
+                    $("#tabs li").one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+                        
+                        $("#tabs li").remove(); //we delete them
+                        appearTabs(tabs);   //we make the new one appear
+                    });
+                }else{   //if we don't have any tabs
+                    
+                    //we create some new one
+                    $("#mainPannel").prepend("<ul id=\"tabs\"></ul>\n");
+                    $("#tabs").addClass("pt-page-moveFromTop"); //we make the tabs background appear smoothly
+                    appearTabs(tabs);
+                }
+                
+                $("#forms").addClass("pt-page-moveToBottom");
+                $("#forms").one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+                    
+                    $("#mainPannel #forms").remove();   //we delete every thing in the mainPannel
+                    $("#mainPannel").append("<div id=\"forms\"></div>"); //we recrate the tabs and forms
+                    for(i=0; i<tabs.length; i++){
+            
+                        $("#forms").append("<div id=\""+tabs[i]+"\"></div>");
+                    }
+                    //we have to launche the subModuleForm event by hand (don't know why)
+                    changeTab("general");
+                });
+
+                
+                
+                
+                function appearTabs(tabs){
+                    
+                    for(i=0; i<tabs.length; i++){
+            
+                        $("#tabs").append("<li id=\"#"+tabs[i]+"\"><a href=\"#"+tabs[i]+"\">"+tabs[i]+"</a></li>");
+                        $("#forms").append("<div id=\""+tabs[i]+"\"></div>");
+                    }
+                    $("#tabs li").addClass("pt-page-moveFromTop");
+                    $("#mainPannel").tabs("refresh");
                 }
 
-                $("#mainPannel").tabs("refresh");
+         
+                                  
             });
         
             request.fail(function(jqXHR, textStatus, errorThrown){
@@ -327,6 +310,73 @@ $config = new Configuration();
             request.always(function(){});
         });
         
+        function changeTab(tabname){
+    
+            //alert("tab");
+            try{if(tabName){oldTabName = tabName;}}catch(e){oldTabName = "general";}
+            //tabName = ui.newTab.attr('id').substr(1);
+            activeSubModule = tabName;
+            
+            var data = {
+                moduleName: activeModule,
+                subModuleName: tabName,
+            }
+                    
+            request = $.ajax({
+                url: "/ajax/getFormSubModule.php",
+                type: "POST",
+                data: data
+            });
+            
+            request.done(function(response, textStatus, jqXHR){
+                
+                //This line is useless as far as we commented the line 14380 in jquery-ui.js
+                //Now jquery don't hide the old tab, we do it with this animation.
+                //$("#forms #"+oldTabName+"_old").show();
+                $("#forms div").addClass("pt-page-flipOutLeft");
+                $("#forms div").one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+    
+                    //alert("delete");
+                    $("#forms div").removeClass("pt-page-flipOutLeft");
+                    $("#forms div").remove();
+                    $("#forms").append("<div id=\""+tabName+"\" class=\"subModuleForm\">"+response+"</div>");
+                    $("#forms #"+tabName).addClass("pt-page-flipInRight");
+                    $("#forms #"+tabName).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+    
+                            $("#forms #"+tabName).removeClass("pt-page-flipInRight");
+                    });
+                });
+                
+                
+                $(".instanceForm").submit(function (){       
+    
+                    var data = $(this).serialize();
+                    data += "&moduleName="+activeModule;
+    
+                    $.ajax({
+                        type    : "POST",
+                        url     : "/ajax/setFormInstance.php",
+                        data    : data,
+                        success : function(data) {
+                            //alert("done");
+                            //opts.onSuccess.call(FORM[0], data);
+                        },
+                        error   : function() {
+                            //opts.onError.call(FORM[0]);
+                        }
+                    });
+                });
+                
+                
+            });
+            
+            request.fail(function(jqXHR, textStatus, errorThrown){
+            
+                alert("error when getting the list of the submodule");
+            });
+            
+            request.always(function(){});
+        }
         
     </script>
 
