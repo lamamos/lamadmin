075456ea9e3c21de8db0c426f714d3dde2282809
karlito139 <karlito@martobre.fr>
2013-10-30 13:12:13 +0100

    Cleared a bit the code.

--- include/controler.js
+++ include/controler.js
@@ -94,6 +94,9 @@ function serviceListCtrl($scope, $http) {
 
 
 
+
+		//Ne pas faire comme cela. Créer un nouvel element dans le DOM qui aura son propre controler
+		//qui se chargera d'aller chercher les données sur le serveur.
 		var donnees = $.param({name: module.name});
 
 		$http({
@@ -109,6 +112,40 @@ function serviceListCtrl($scope, $http) {
 				var tabs= response.split(",");                 
 				
 				if($("#mainPannel #tabs").length > 0){   //if we have a tabs element
+				    
+			        $("#tabs li").remove(); //we delete them
+			        appearTabs(tabs);   //we make the new one appear
+				}else{   //if we don't have any tabs
+				    
+				    //we create some new one
+				    $("#mainPannel").prepend("<ul id=\"tabs\"></ul>\n");
+				    $("#tabs").addClass("pt-page-moveFromTop"); //we make the tabs background appear smoothly
+				    appearTabs(tabs);
+				}
+
+			    $("#mainPannel #forms").remove();   //we delete every thing in the mainPannel
+			    $("#mainPannel").append("<div id=\"forms\"></div>"); //we recrate the tabs and forms
+			    for(i=0; i<tabs.length; i++){
+	
+			        $("#forms").append("<div id=\""+tabs[i]+"\"></div>");
+			    }
+			    //we have to launche the subModuleForm event by hand (don't know why)
+			    changeTab("general");
+
+
+				function appearTabs(tabs){
+				    
+				    for(i=0; i<tabs.length; i++){
+		
+				        $("#tabs").append("<li id=\"#"+tabs[i]+"\"><a href=\"#"+tabs[i]+"\">"+tabs[i]+"</a></li>");
+				        $("#forms").append("<div id=\""+tabs[i]+"\"></div>");
+				    }
+				    $("#tabs li").addClass("pt-page-moveFromTop");
+				    $("#mainPannel").tabs("refresh");
+				}
+
+
+				/*if($("#mainPannel #tabs").length > 0){   //if we have a tabs element
 				    
 				    $("#tabs li").addClass("pt-page-moveToBottom"); //we make the tab deseapear
 				    //when the tabs have desapeared
@@ -150,7 +187,7 @@ function serviceListCtrl($scope, $http) {
 				    }
 				    $("#tabs li").addClass("pt-page-moveFromTop");
 				    $("#mainPannel").tabs("refresh");
-				}
+				}*/
 		
 		         
 
