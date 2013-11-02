c2136ddfd03cec257c77b4e2cd3b6b702be0e033
karlito139 <karlito@martobre.fr>
2013-11-02 10:27:42 +0100

    Cleared a bit of useless code.

--- include/formsControlers.js
+++ include/formsControlers.js
@@ -95,30 +95,6 @@ function formCtrl($scope, $http){
 			})
 		;
 
-/*
-		$(".instanceForm").submit(function (){
-
-			var data = $(this).serialize();
-			data += "&moduleName="+activeModule;
-
-			$.ajax({
-				type : "POST",
-				url : "/ajax/setFormInstance.php",
-				data : data,
-
-				success : function(data) {
-					refresh();
-					//opts.onSuccess.call(FORM[0], data);
-				},
-				error : function() {
-					alert("Error when commitin the modif on this module.");
-					//opts.onError.call(FORM[0]);
-				}
-			});
-		});
-*/
-
-
 	}
 
 	$scope.delete = function(){
