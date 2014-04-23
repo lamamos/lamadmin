780e5355f060d959eea7fe7e06688476e0548406
karlito139 <karlito@martobre.fr>
2014-04-23 21:51:45 +0200

    Removed the errors that the firefox console displayd.

--- include/configuration.php
+++ include/configuration.php
@@ -18,5 +18,5 @@ You should have received a copy of the GNU General Public License
 along with Lamadmin.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-    $PathToRexConfiguration = "/etc/lamamos/rex/";
+    $PathToRexConfiguration = "/home/karlito/creation/lamamos/test_rexify/";
 ?>
