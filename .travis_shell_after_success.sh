1330bcde78fbb8ed0e71136378a03d5065733b5a
karlito139 <karlito@martobre.fr>
2014-05-05 15:34:32 +0200

    Changed the script.

--- .travis_shell_after_success.sh
+++ .travis_shell_after_success.sh
@@ -6,7 +6,7 @@ echo "TRAVIS_PHP_VERSION: $TRAVIS_PHP_VERSION"
 echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
  
-if [ "$TRAVIS_REPO_SLUG" == "lamadmin" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_PHP_VERSION" == "5.5" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_PHP_VERSION" == "5.5" ]; then
  
   echo -e "Publishing Doxygen...\n"
   ## Copie de la documentation generee dans le $HOME
