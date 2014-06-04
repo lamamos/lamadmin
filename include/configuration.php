ad4806558044d7b86092e910d454b3744d6b90c0
karlito139 <karlito@martobre.fr>
2014-06-04 21:14:00 +0200

    putted back the config file as it was

--- include/configuration.php
+++ include/configuration.php
@@ -18,5 +18,5 @@ You should have received a copy of the GNU General Public License
 along with Lamadmin.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-    $PathToRexConfiguration = "/home/karlito/Downloads/rex/";
+    $PathToRexConfiguration = "/etc/lamamos/rex/";
 ?>
