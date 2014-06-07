26d37c39d06864c73a5b0c48e4aec9f17326bf65
karlito139 <karlito@martobre.fr>
2014-06-07 14:35:39 +0200

    don't check the installed libraries

--- .travis_syntax_check.sh
+++ .travis_syntax_check.sh
@@ -4,7 +4,7 @@ find . \( -iname "*.php" ! -path "./vendor*" \) -print0 | xargs -0 -n1 php -l
 phpSyntaxContainsErrors=`echo $?`
 
 
-find . \( -iname "*.js" ! -path "./vendor*" ! -path "./doc*" \) -print0 | xargs -0 -n1 jshint
+find . \( -iname "*.js" ! -path "./vendor*" ! -path "./doc*" ! -iname "*.min.js" \) -print0 | xargs -0 -n1 jshint
 jsSyntaxContainsErrors=`echo $?`
 
 
