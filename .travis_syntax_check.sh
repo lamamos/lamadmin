46e07d4596cf236bc7a4ccd341e6e062e8de3c52
karlito139 <karlito@martobre.fr>
2014-06-07 12:29:47 +0200

    added a return of the test of the syntax

--- .travis_syntax_check.sh
+++ .travis_syntax_check.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 find . -iname "*.php" -exec php -l "{}" \;
+phpSyntaxContainsErrors=`echo $?`
+
+
+if [ phpSyntaxContainsErrors -eq 0 ]
+then
+	return 0
+else
+	return phpSyntaxContainsErrors
+fi
